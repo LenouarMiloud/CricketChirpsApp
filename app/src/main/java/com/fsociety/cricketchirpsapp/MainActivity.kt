5fd2aaab22664655386541898e441661ebc6f201
LenouarMiloud <lenouarmiloud@gmail.com>
2021-12-17 23:11:38 +0100

    complete the code of the app

--- app/src/main/java/com/fsociety/cricketchirpsapp/MainActivity.kt
+++ app/src/main/java/com/fsociety/cricketchirpsapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.fsociety.cricketchirpsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
+
+    var chirp:Int?= 0
+    var temp:Double?=0.0
+    val format = DecimalFormat("#0.00")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calculateButton.setOnClickListener(View.OnClickListener {
+            if(EditTextTemperature.text.toString().isEmpty()){
+                Toast.makeText(this,"please enter all fields",Toast.LENGTH_LONG).show()
+            }else{
+                chirp = Integer.parseInt(EditTextTemperature.text.toString())
+                temp = (chirp!! / 3.0 ) +4
+                resultText.text = "the approximate temperature outside is ${format.format(temp)} degrees Celsius"
+
+            }
+        })
+
+
+
     }
 }
